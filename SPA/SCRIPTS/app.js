0e5edaa3f1e2d5c4deb94662e8ab3a8b0859fea6
Ebosch <nico.bosch21@gmail.com>
2021-11-21 20:59:35 -0300

    Pruebas de inicio

--- SPA/SCRIPTS/app.js
+++ SPA/SCRIPTS/app.js
@@ -11,7 +11,33 @@ app.config(function($stateProvider, $urlRouterProvider){
         templateUrl: 'home.html',
         controller: 'HomeController',
         controllerAs: 'home'
-    });
+    })
+    .state('listadoProductos', {
+        url: '/listadoProductos',   
+        templateUrl: 'listadoProductos.html',
+        controller: 'ListadoProductosController',
+        controllerAs: 'listadoProductos'
+        
+    })
+    .state('carrito', {
+        url: '/carrito',
+        templateUrl: 'carrito.html',
+        controller: 'carritoController',
+        controllerAs: 'carrito'
+    })
+    .state('beneficioUsuario', {
+        url: '/beneficioUsuario',
+        templateUrl: 'beneficioUsuario.html',
+        controller: 'beneficioUsuarioController',
+        controllerAs: 'beneficioUsuario'
+    })
+    .state('detalleProducto', {
+        url: '/detalleProducto',
+        templateUrl: 'detalleProducto.html',
+        controller: 'detalleProductoController',
+        controllerAs: 'detalleProducto'
+    })
+    ;
 
     $urlRouterProvider.otherwise('/home')
 })
