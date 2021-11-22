9363b13c2af98dfb5d9817d657fe9ca881fec268
Ebosch <nico.bosch21@gmail.com>
2021-11-21 21:00:01 -0300

    cambios de inicio

--- SPA/SCRIPTS/app.js
+++ SPA/SCRIPTS/app.js
@@ -7,32 +7,32 @@ var app = angular.module('e-commerce',[
 app.config(function($stateProvider, $urlRouterProvider){
     $stateProvider
     .state('home', {
-        url:'/home',
+        url:'spa/views//home',
         templateUrl: 'home.html',
         controller: 'HomeController',
         controllerAs: 'home'
     })
     .state('listadoProductos', {
-        url: '/listadoProductos',   
+        url: 'spa/views//listadoProductos',   
         templateUrl: 'listadoProductos.html',
         controller: 'ListadoProductosController',
         controllerAs: 'listadoProductos'
         
     })
     .state('carrito', {
-        url: '/carrito',
+        url: 'spa/views//carrito',
         templateUrl: 'carrito.html',
         controller: 'carritoController',
         controllerAs: 'carrito'
     })
     .state('beneficioUsuario', {
-        url: '/beneficioUsuario',
+        url: 'spa/views//beneficioUsuario',
         templateUrl: 'beneficioUsuario.html',
         controller: 'beneficioUsuarioController',
         controllerAs: 'beneficioUsuario'
     })
     .state('detalleProducto', {
-        url: '/detalleProducto',
+        url: 'spa/views//detalleProducto',
         templateUrl: 'detalleProducto.html',
         controller: 'detalleProductoController',
         controllerAs: 'detalleProducto'
